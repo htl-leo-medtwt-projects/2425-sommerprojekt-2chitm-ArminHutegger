0120ba8f7d1a4420fb11f722941c36aa66314f50
Armin Hutegger <a.hutegger@students.htl-leonding.ac.at>
2025-03-27 09:49:45 +0100

    html und css

---
+++ project/files/StarWiki.js
@@ -0,0 +1,15 @@
+
+function Pyro(){
+    document.documentElement.style.setProperty("--main-color","#DB5A5A")
+    document.documentElement.style.setProperty("--second-main","#DB5A5A")
+    document.getElementById("head1").src = "../img/pyro1.jpeg"
+    document.getElementById("box2").style.opacity = 1;
+    document.getElementById("box1").style.opacity = 0.4;
+}
+function Stanton(){
+    document.documentElement.style.setProperty("--main-color","#7D9BDB")
+    document.documentElement.style.setProperty("--second-main","#A7B5D2")
+    document.getElementById("head1").src = "../img/stanton2.jpg"
+    document.getElementById("box2").style.opacity = 0.4;
+    document.getElementById("box1").style.opacity = 1;
+}
