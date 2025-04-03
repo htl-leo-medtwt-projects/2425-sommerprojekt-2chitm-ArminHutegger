07d398f2d78e266fd45b2ef560d9e452cd28afbd
Armin Hutegger <a.hutegger@students.htl-leonding.ac.at>
2025-04-03 14:31:26 +0200

    c2

--- project/files/StarWiki.js
+++ project/files/StarWiki.js
@@ -1,15 +1,39 @@
-
-function Pyro(){
-    document.documentElement.style.setProperty("--main-color","#DB5A5A")
-    document.documentElement.style.setProperty("--second-main","#DB5A5A")
-    document.getElementById("head1").src = "../img/pyro1.jpeg"
-    document.getElementById("box2").style.opacity = 1;
-    document.getElementById("box1").style.opacity = 0.4;
+let swiched = false;
+loadMode()
+function swich(){
+    swiched = true;
+    let mode = localStorage.getItem("mode");
+    if(mode == "Stanton"){
+        localStorage.setItem("mode", "Pyro");
+        loadMode()
+    }else{
+        localStorage.setItem("mode", "Stanton");
+        loadMode()
+    }
 }
-function Stanton(){
-    document.documentElement.style.setProperty("--main-color","#7D9BDB")
-    document.documentElement.style.setProperty("--second-main","#A7B5D2")
-    document.getElementById("head1").src = "../img/stanton2.jpg"
-    document.getElementById("box2").style.opacity = 0.4;
-    document.getElementById("box1").style.opacity = 1;
+function loadMode() {
+    try{
+        let mode = localStorage.getItem("mode");
+        if (mode === "Stanton") {
+            document.documentElement.style.setProperty("--main-color", "#7D9BDB");
+            document.documentElement.style.setProperty("--second-main", "#A7B5D2");
+            document.getElementById("head1").src = "../img/stanton2.jpg";
+    
+            Array.from(document.getElementsByClassName("box1")).forEach(element => {
+                element.style.backgroundImage = 'url("../img/mode1.jpg")';
+            });
+    
+        } else if (mode === "Pyro") {
+            document.documentElement.style.setProperty("--main-color", "#DB5A5A");
+            document.documentElement.style.setProperty("--second-main", "#DB5A5A");
+            document.getElementById("head1").src = "../img/pyro1.jpeg";
+    
+            Array.from(document.getElementsByClassName("box1")).forEach(element => {
+                element.style.backgroundImage = 'url("../img/mode2.webp")';
+            });
+        }
+    }catch{
+
+    }
+
 }
