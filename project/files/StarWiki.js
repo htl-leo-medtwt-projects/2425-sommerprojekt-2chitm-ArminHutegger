3ede889e71e8212e8354837a66fcf7dc6e7b9dcd
Armin Hutegger <a.hutegger@students.htl-leonding.ac.at>
2025-04-04 11:32:16 +0200

    c3

--- project/files/StarWiki.js
+++ project/files/StarWiki.js
@@ -15,22 +15,24 @@ function loadMode() {
     try{
         let mode = localStorage.getItem("mode");
         if (mode === "Stanton") {
+            Array.from(document.getElementsByClassName("box1")).forEach(element => {
+                element.style.backgroundImage = 'url("../img/mode1.jpg")';
+            });
             document.documentElement.style.setProperty("--main-color", "#7D9BDB");
             document.documentElement.style.setProperty("--second-main", "#A7B5D2");
             document.getElementById("head1").src = "../img/stanton2.jpg";
     
-            Array.from(document.getElementsByClassName("box1")).forEach(element => {
-                element.style.backgroundImage = 'url("../img/mode1.jpg")';
-            });
+            
     
         } else if (mode === "Pyro") {
+            Array.from(document.getElementsByClassName("box1")).forEach(element => {
+                element.style.backgroundImage = 'url("../img/mode2.webp")';
+            });
             document.documentElement.style.setProperty("--main-color", "#DB5A5A");
             document.documentElement.style.setProperty("--second-main", "#DB5A5A");
             document.getElementById("head1").src = "../img/pyro1.jpeg";
     
-            Array.from(document.getElementsByClassName("box1")).forEach(element => {
-                element.style.backgroundImage = 'url("../img/mode2.webp")';
-            });
+            
         }
     }catch{
 
