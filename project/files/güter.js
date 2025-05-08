2bda7f888947459ecee779a414eee6f55b41529e
Armin Hutegger <a.hutegger@students.htl-leonding.ac.at>
2025-05-08 07:18:47 +0200

    c17

---
+++ project/files/güter.js
@@ -0,0 +1,17 @@
+let güter =[
+    { "name": "Widow", "value_uec": 4200.0, "legality": "illegal" },
+    { "name": "Neon", "value_uec": 3200.0, "legality": "illegal" },
+    { "name": "Altruciatoxin", "value_uec": 2900.0, "legality": "illegal" },
+    { "name": "SLAM", "value_uec": 2700.0, "legality": "illegal" },
+    { "name": "E'tam", "value_uec": 2500.0, "legality": "illegal" },
+    { "name": "Quantanium", "value_uec": 8800.0, "legality": "legal" },
+    { "name": "Agricium", "value_uec": 2750.0, "legality": "legal" },
+    { "name": "Laranite", "value_uec": 2560.0, "legality": "legal" },
+    { "name": "Bexalite", "value_uec": 2500.0, "legality": "legal" },
+    { "name": "Taranite", "value_uec": 2400.0, "legality": "legal" },
+    { "name": "Hephaestanite", "value_uec": 1750.0, "legality": "legal" },
+    { "name": "Titanium", "value_uec": 720.0, "legality": "legal" },
+    { "name": "Tungsten", "value_uec": 650.0, "legality": "legal" },
+    { "name": "Gold", "value_uec": 560.0, "legality": "legal" },
+    { "name": "Copper", "value_uec": 410.0, "legality": "legal" }
+  ]
